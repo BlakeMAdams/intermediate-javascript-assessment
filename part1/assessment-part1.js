cfda65fd0aa1ae5ad3947e0fe1efc25ac7605edd
BlakeMAdams <blakemadams@gmail.com>
2017-10-09 16:49:45 -0600

    assessment 1-4 complete, 5th still left to finish solving

--- part1/assessment-part1.js
+++ part1/assessment-part1.js
@@ -43,27 +43,30 @@ function daBears(){
 // Which function(s) access the "chair" variable and get "Too Big!"
 // (Delete wrong answers, leave correct ones)
 
-var fairyTale1 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
+//var fairyTale1 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
+var fairyTale1 = ["papaBear", "mamaBear"];
 
 // Which function(s) access the "feeling" variable and get "Hungry"
 // (Delete wrong answers, leave correct ones)
 
-var fairyTale2 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
+//var fairyTale2 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
+var fairyTale2 = ["goldilocks"];
 
 // Which function(s) access the "porridge" variable and get "Too Cold!"
 // (Delete wrong answers, leave correct ones)
 
-var fairyTale3 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
+//var fairyTale3 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
+var fairyTale3 = ["mamaBear"];
 
 // Which function(s) access the "sleepy" variable and get undefined
 // (Delete wrong answers, leave correct ones)
 
-var fairyTale4 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
+var fairyTale4 = ["daBears", "papaBear", "mamaBear", "goldilocks"];
 
 // Which function(s) access the isFurry variable and get true
 // (Delete wrong answers, leave correct ones)
 
-var fairyTale5 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
+var fairyTale5 = ["daBears", "papaBear", "mamaBear", "babyBear"];
 
 
 // *************
@@ -83,6 +86,17 @@ var fairyTale5 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
 
 // CODE HERE...
 
+function Vehicle(){
+  this.gasRemaining = 100;
+}
+Vehicle.prototype.drive = function() {this.gasRemaining -= 25}
+
+var charger = new Vehicle();
+var mustang = new Vehicle();
+
+charger.drive();
+mustang.drive();
+mustang.drive();
 
 
 
@@ -104,7 +118,18 @@ var fairyTale5 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
 // Your method may be passed punctuation, numbers or other non-letter characters
 // and should neither modify them nor break when encountering them.
 
+String.prototype.grammarPolice = function(){
 
+  let lowerString = this.toLowerCase();
+  var array = lowerString.split(' ');
+  var upperCase = [];
+  for (let i =0; i<array.length; i++){
+   
+    upperCase.push(array[i].charAt(0).toUpperCase() + array[i].slice(1))
+   
+  }
+  return upperCase.join(' ');
+}
 
 
 // CODE HERE...
@@ -127,7 +152,14 @@ var fairyTale5 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
 
 // CODE HERE...
 
+function valueType(a,b){
+  if(a === b){
+    return "Exactly the same";
+  } else if(a == b){
+    return "Same value, different types";
+  } else { return "Different values"}
 
+}
 
 // *************
 // * PROBLEM 5 *
@@ -141,3 +173,7 @@ var fairyTale5 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
 var theAnswer = "Unknown";
 
 // CODE HERE...
+
+// var promiseCatcher = function(promise).then( theAnswer = Promise.resolve)
+// var promiseCatcher = function(promise){ (promise)=> { theAnswer = promise.resolve }}
+// function promiseCatcher(promise){var yes = promise.resolve}.then( theAnswer = yes)
